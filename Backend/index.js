753211155548b89e57e31bad1ed807e81b9f5835
Manini21 <maninichhabra@gmail.com>
2026-02-14 19:10:00 +0530

    update

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin:[ "http://localhost:5173",
-    "https://your-app.vercel.app"]
+    "https://your-app.vercel.app"],
     methods: ["GET", "POST"],
   },
 });
